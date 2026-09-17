61f3aceb9e8c159f15ab3055df299f851524f296
Knight <hknight4@una.edu>
2026-09-17 13:43:44 -0500

    cxn to mongo

---
+++ server/mongo.js
@@ -0,0 +1,32 @@
+// OLD WAY - require('dotenv').config();
+import 'dotenv/config';
+
+// OLD WAY - const { MongoClient, ServerApiVersion } = require('mongodb');
+import { MongoClient, ServerApiVersion } from 'mongodb';
+// const uri = "mongodb+srv://fullstack-hayden:OUtGDefQli5voQCS@cluster0.eaqcsre.mongodb.net/?appName=Cluster0";
+
+const uri = process.env.MONGO_URI;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
